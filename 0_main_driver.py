f82b4d62fadcaa549d3427afaaa6565ea70a42aa
Fatality-GAW <129615568+Fatality-GAW@users.noreply.github.com>
2023-04-30 21:03:16 -0300

    Via Upload
    Added latest CSV's
    
    Added code some small code changes.

--- 0_main_driver.py
+++ 0_main_driver.py
@@ -8,6 +8,7 @@ import os
 import subprocess
 from datetime import datetime, timedelta
 import time
+import pytz
 
 RETRY_SECONDS = 3600  # every hour
 
@@ -15,27 +16,33 @@ GAW_sticky_logs_objects = './WorkingCSVs/GAW_sticky_logs_objects.csv'
 
 GAW_sticky_posts_objects = './WorkingCSVs/GAW_sticky_posts_objects.csv'
 
-sticky_logs_size = os.path.getsize(GAW_sticky_logs_objects)
-sticky_posts_size = os.path.getsize(GAW_sticky_posts_objects)
+sticky_logs_size = 0
+if os.path.exists(GAW_sticky_logs_objects):
+    sticky_logs_size = os.path.getsize(GAW_sticky_logs_objects)
+
+sticky_posts_size = 0
+if os.path.exists(GAW_sticky_posts_objects):
+    sticky_posts_size = os.path.getsize(GAW_sticky_posts_objects)
 
 
 while True:
 
     print(f'SCRAPING STICKY LOGS:')
     subprocess.run(['python3', '1_scrape_gaw_sticky_logs_objects.py'])
-    if sticky_logs_size != os.path.getsize(GAW_sticky_logs_objects):
+    if os.path.exists(GAW_sticky_logs_objects) and sticky_logs_size != os.path.getsize(GAW_sticky_logs_objects):
         print(f'SCRAPING STICKY POSTS:')
         subprocess.run(['python3', '2_scrape_gaw_sticky_post_objects.py'])
     else:
         print('NO NEW STICKIES TO SCRAPE!')
-    if sticky_posts_size != os.path.getsize(GAW_sticky_posts_objects):
+    if os.path.exists(GAW_sticky_posts_objects) and sticky_posts_size != os.path.getsize(GAW_sticky_posts_objects):
         print(f'GENERATING MONTHLY CSV FILES:')
         subprocess.run(['python3', '3_split_data_into_yyyy.mm.csv_files.py'])
         print(f'GENERATING STICKIES RELOADED HTML FILES:')
         subprocess.run(['python3', '4_gaw_stickies_reloaded_page_generator.py'])
 
     retryTime = datetime.now() + timedelta(seconds=RETRY_SECONDS)
-    print(f"\nWaiting to run again @ [{retryTime}]\r\n(CTRL+C to quit)")
+    retryTimeGMT = datetime.now(pytz.timezone('GMT')) + timedelta(seconds=RETRY_SECONDS)
+    print(f"\nWaiting to run again @\r\n\t[{retryTime}](LOCAL)\r\n\t[{retryTimeGMT}](GMT)\r\n(CTRL+C to quit)")
     try:
         time.sleep(RETRY_SECONDS)
     except:
